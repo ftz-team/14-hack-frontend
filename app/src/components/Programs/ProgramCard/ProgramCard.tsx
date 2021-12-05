d025336945e5ef936f0a9430d7dda225967dddef
Ramaz Omsarashvili <78808062+oramaz@users.noreply.github.com>
2021-12-05 14:24:24 +0300

    fixes

--- app/src/components/Programs/ProgramCard/ProgramCard.tsx
+++ app/src/components/Programs/ProgramCard/ProgramCard.tsx
@@ -1,19 +1,15 @@
-import { Card, CardDescriptions, CardHeader } from "./ProgramCard.styled"
+import Router from "next/router";
+import { Card, CardDescriptions, CardHeader } from "./ProgramCard.styled";
 
-export const ProgramCard : React.FC<any> = ({card}) => {
-    console.log(card);
-    
-    return (
-        <>
-            <Card background={card.image}>
-                <CardHeader>
-                    {card.name}
-                </CardHeader>
-                <CardDescriptions>
-                    {card.description}
-                </CardDescriptions>
-            </Card>
-        </>
-    )
+export const ProgramCard: React.FC<any> = ({ card }) => {
+  console.log(card);
 
-}
+  return (
+    <>
+      <Card background={card.image} onClick={() => Router.push("/programs/1")}>
+        <CardHeader>{card.name}</CardHeader>
+        <CardDescriptions>{card.description}</CardDescriptions>
+      </Card>
+    </>
+  );
+};
--- app/src/layouts/AdminLayout/AdminLayout.tsx
+++ app/src/layouts/AdminLayout/AdminLayout.tsx
@@ -1,35 +1,50 @@
 import React from "react";
 import { Logo } from "../../components/Logo/Logo";
-import { Content, MainContainer, Navigation, NavigationItem } from "./AdminLayout.styled";
-import home from '../../../src/assets/img/home.png'
+import {
+  Content,
+  MainContainer,
+  Navigation,
+  NavigationItem,
+} from "./AdminLayout.styled";
+import home from "../../../src/assets/img/home.png";
 import { Programs } from "../../components/Programs/Porgrams";
 import { UsersList } from "../../components/UsersList/UsersList";
 
-export const AdminLayout : React.FC = ({children}) => {
+export const AdminLayout: React.FC = ({ children }) => {
+  const [activeTab, setActiveTab] = React.useState(0);
 
-    const [activeTab, setActiveTab] = React.useState(0);
-
-    return (
-        <> 
-            <MainContainer>
-                <Navigation>
-                    <Logo/>
-                    <NavigationItem active={activeTab==0} onClick={() => setActiveTab(0)}>
-                        <img src={home.src}/>
-                        <p>Программы</p>
-                    </NavigationItem>
-                    <NavigationItem active={activeTab==1} onClick={() => setActiveTab(1)}>
-                        <img src={home.src}/>
-                        <p>Ученики</p>
-                    </NavigationItem>
-                </Navigation>
-                <Content>
-                    {children ? children:
-                    activeTab == 0 ? <Programs/>:
-                    activeTab == 1 ? <UsersList/> : null  }
-                </Content>
-            </MainContainer>
-        </>
-    )
-
-}
+  return (
+    <>
+      <MainContainer>
+        <Navigation>
+          <Logo />
+          <NavigationItem
+            active={activeTab == 0}
+            onClick={() => setActiveTab(0)}
+          >
+            <img src={home.src} />
+            <p>Программы</p>
+          </NavigationItem>
+          <NavigationItem
+            active={activeTab == 1}
+            onClick={() => {
+              setActiveTab(1);
+            }}
+          >
+            <img src={home.src} />
+            <p>Ученики</p>
+          </NavigationItem>
+        </Navigation>
+        <Content>
+          {children ? (
+            children
+          ) : activeTab == 0 ? (
+            <Programs />
+          ) : activeTab == 1 ? (
+            <UsersList />
+          ) : null}
+        </Content>
+      </MainContainer>
+    </>
+  );
+};
